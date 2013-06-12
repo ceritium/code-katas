7ca68e5db15b11cbee7e06a9acbca8b5e9ed407b
José Galisteo <ceritium@gmail.com>
2013-06-12 13:13:32 +0200

    c_tree

---
+++ lua-tree/c_tree.lua
@@ -0,0 +1,2 @@
+require 'tree'
+print(Tree.new(arg[1] or 3):draw())
--- lua-tree/tree.lua
+++ lua-tree/tree.lua
@@ -24,24 +24,3 @@ function Tree:new_line(floor)
 end
 
 
-$ tsc -f tree_spec.lua
-------------------------------------------------------------------------
-Tree:
-A Tree with height of 1:
-  height should be 1                                                 [P]
-  should draw '#'                                                    [P]
-  should draw the tree '#'                                           [P]
-A Tree with height of 2:
-  height should be 2                                                 [P]
-  should draw '_#'                                                   [P]
-  should draw '###'                                                  [P]
-  should draw the tree '_#
-###
-'                                     [P]
-A Tree with height of 3:
-  height should be 3                                                 [P]
-  should draw '__#'                                                  [P]
-  should draw '_###'                                                 [P]
-  should draw '#####'                                                [P]
-------------------------------------------------------------------------
-11 tests 11 passed 11 assertions 0 failed 0 errors 0 unassertive 0 pending
